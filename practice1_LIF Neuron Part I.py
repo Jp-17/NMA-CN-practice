2455cdc70ac3d29cff7a8f230789adce15ed2ec0
JiangP <jiangp17@mails.tsinghua.edu.cn>
2022-01-30 22:54:06 +0800

    revise

--- practice1_LIF Neuron Part I.py
+++ practice1_LIF Neuron Part I.py
@@ -14,37 +14,155 @@ i_mean = 25e-11  # ampere
 
 print(t_max, dt, tau, el, vr, vth, r, i_mean)
 
-# Loop for 10 steps, variable 'step' takes values from 0 to 9
-for step in range(10):
-    # Compute value of t
-    t = step * dt
+# Set random number generator
+np.random.seed(2020)
 
-    # Compute value of i at this time step
-    i = i_mean * (1 + np.sin(2 * np.pi * t / 0.01))
+# Initialize step_end and n
+step_end = int(t_max / dt)
+n = 50
 
-    # Print value of i
-    print(f'{t:.3f}', f'{i:.4e}')
+# Intiatialize the list v_n with 50 values of membrane leak potential el
+v_n = [el] * n
 
-# #################################################
-# ## TODO for students: fill out compute v code ##
-# # Fill out code and comment or remove the next line
-# raise NotImplementedError("Student exercise: You need to fill out code to compute v")
-# #################################################
-
-# Initialize step_end and v0
-step_end = 10
-v = el
+# Initialize the figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
 
 # Loop for step_end steps
 for step in range(step_end):
+
     # Compute value of t
     t = step * dt
 
-    # Compute value of i at this time step
-    i = i_mean * (1 + np.sin((t * 2 * np.pi) / 0.01))
+    # Loop for n simulations
+    for j in range(0, n):
 
-    # Compute v
-    v = v + (el - v + r * i) * dt / tau
+        # Compute value of i at this time step
+        i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2 * np.random.random() - 1))
 
-    # Print value of t and v
-    print(f"{t:.3f} {v:.4e}")
+        # Compute value of v for this simulation
+        v_n[j] = v_n[j] + (dt / tau) * (el - v_n[j] + r*i)
+
+    # Compute sample mean
+    v_mean = np.mean(v_n)
+
+    # Initialize a list `v_var_n` with the contribution of each V_n(t) to
+    # Var(t) with a list comprehension over values of v_n
+    v_var_n = [(x - v_mean)**2 for x in v_n]
+
+    # Compute sample variance v_var by summing the values of v_var_n with sum and dividing by n-1
+    v_var = np.sum(v_var_n) / (n - 1)
+
+    # Compute the standard deviation v_std with the function np.sqrt
+    v_std = v_var**0.5
+
+    # Plot simulations
+    plt.plot(n*[t], v_n, 'k.', alpha=0.1)
+
+    # Plot sample mean using alpha=0.8 and'C0.' for blue
+    plt.plot(t, v_mean, 'C0.', alpha=0.8, markersize=10)
+
+    # Plot mean + standard deviation with alpha=0.8 and argument 'C7'
+    plt.plot(t, v_mean + v_std, 'C7.', alpha=0.8)
+
+    # Plot mean - standard deviation with alpha=0.8 and argument 'C7'
+    plt.plot(t, v_mean - v_std, 'C7.', alpha=0.8)
+
+# Display plot
+plt.show()
+
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, t_range, v
+step_end = int(t_max / dt) - 1
+t_range = np.linspace(0, t_max, num=step_end, endpoint=False)
+v = el * np.ones(step_end)
+
+# Simulate current over time
+i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2 * np.random.random(step_end) - 1))
+
+# Loop for step_end steps
+for step in range(1, step_end):
+
+  # Compute v as function of i
+  v[step] = v[step - 1] + (dt / tau) * (el - v[step - 1] + r*i[step - 1])
+
+
+# Plot membrane potential
+plt.figure()
+plt.title('$V_m$ with random I(t)')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v, 'k.')
+plt.show()
+
+
+# Loop for step_end values of i using enumerate
+for step, i_step in enumerate(i):
+
+  # Skip first iteration
+  if step==0:
+    continue
+
+  # Compute v as function of i using i_step
+  v[step] = v[step - 1] + (dt / tau) * (el - v[step - 1] + r*i_step)
+
+# Plot figure
+plt.figure()
+plt.title('$V_m$ with random I(t)')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v, 'k')
+plt.show()
+
+# Initialize step_end, n, t_range, v and i
+step_end = int(t_max / dt)
+n = 50
+t_range = np.linspace(0, t_max, num=step_end)
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max/dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
+
+# Loop for step_end - 1 steps
+for step in range(1, step_end):
+
+   # Compute v_n
+   v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r*i[:, step - 1])
+
+# Plot figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v_n.T, 'k', alpha=0.1)
+plt.show()
+
+
+# Compute sample mean (use np.mean)
+v_mean = np.mean(v_n, axis=0)
+
+# Compute sample standard deviation (use np.std)
+v_std = (np.sum((v_n - v_mean)**2, axis=0) / (n-1))**0.5
+# v_std = np.std(v_n, axis=0)
+
+# Plot figure
+plt.figure()
+plt.title('Multiple realizations of $V_m$')
+plt.xlabel('time (s)')
+plt.ylabel('$V_m$ (V)')
+
+plt.plot(t_range, v_n.T, 'k', alpha=0.3)
+
+plt.plot(t_range, v_n[-1], 'k', alpha=0.3, label='V(t)')
+plt.plot(t_range, v_mean, 'C0', alpha=0.8, label='mean')
+plt.plot(t_range, v_mean+v_std, 'C7', alpha=0.8)
+plt.plot(t_range, v_mean-v_std, 'C7', alpha=0.8, label='mean $\pm$ std')
+
+plt.legend()
+plt.show()
