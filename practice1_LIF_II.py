0ca23fccb392afe9d11b9b1d57d9d59a8304e595
JiangP <jiangp17@mails.tsinghua.edu.cn>
2022-02-03 00:39:31 +0800

    practice LIF II

--- practice1_LIF_II.py
+++ practice1_LIF_II.py
@@ -143,6 +143,7 @@ plt.ylabel('rate (Hz)')
 plt.tight_layout()
 plt.show()
 
+
 def plot_all(t_range, v, raster=None, spikes=None, spikes_mean=None):
     """
   Plots Time evolution for
@@ -204,6 +205,7 @@ def plot_all(t_range, v, raster=None, spikes=None, spikes_mean=None):
     plt.tight_layout()
     plt.show()
 
+
 # Set random number generator
 np.random.seed(2020)
 
@@ -254,3 +256,43 @@ spikes_mean = spikes_n / n
 # Plot multiple realizations of Vm, spikes and mean spike rate
 plot_all(t_range, v_n, spikes=spikes, spikes_mean=spikes_mean)
 
+# #################################################
+# ## TODO for students: make a raster ##
+# # Fill out function and remove
+# raise NotImplementedError("Student exercise: make a raster ")
+# #################################################
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, t_range, n, v_n and i
+t_range = np.arange(0, t_max, dt)
+step_end = len(t_range)
+n = 500
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max / dt) ** (0.5) * (2 * np.random.random([n, step_end]) - 1))
+
+# Initialize binary numpy array for raster plot
+raster = np.zeros([n, step_end])
+
+# Loop over time steps
+for step, t in enumerate(t_range):
+
+    # Skip first iteration
+    if step == 0:
+        continue
+
+    # Compute v_n
+    v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r * i[:, step])
+
+    # Initialize boolean numpy array `spiked` with v_n > v_thr
+    spiked = (v_n[:, step] >= vth)
+
+    # Set relevant values of v_n to v_reset using spiked
+    v_n[spiked, step] = vr
+
+    # Set relevant elements in raster to 1 using spiked
+    raster[spiked, step] = 1
+
+# Plot multiple realizations of Vm, spikes and mean spike rate
+plot_all(t_range, v_n, raster)
